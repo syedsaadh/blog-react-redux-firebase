c0606268618dfb245dfde558c5da4df06f13889a
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-21 02:17:30 +0530

    Article Page Updated with ArticleFetch actions

--- src/containers/ArticlePage.js
+++ src/containers/ArticlePage.js
@@ -1,35 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as markdown from 'markdown-it';
 import renderHtml from 'react-render-html';
+import { fetchArticle } from '../actions'
 var md = new markdown({
     html: true,
-  linkify: true,
-  typographer: true
+    linkify: true,
+    typographer: true
 })
-class ArticlePage extends Component{
-    renderPost(){
-        return renderHtml(md.render(this.props.post.desc || 'Select a Article'));
+class ArticlePage extends Component {
+    renderPost() {
+        const { post } = this.props;
+        return renderHtml(md.render(post.desc || ''));
     }
-    render(){
-        console.log(this.props)
-        return(
-            <div>    
+    componentDidMount() {
+        const { dispatch, post, match } = this.props;
+        if (!post.desc) {
+            dispatch(fetchArticle(match.params.slug))
+        }
+    }
+    render() {
+        const { isFetching } = this.props;
+        return (
+            <div>
+                {
+                    isFetching && <h3>Loading..</h3>
+                }
                 <h1>{this.props.post.title}</h1>
                 <div>
-                    { this.renderPost()}
+                    {
+                        !isFetching && this.renderPost()
+                    }
                 </div>
             </div>
         )
     }
 }
 const mapStateToProps = state => {
-    const { selectedArticle } = state;
+    const { selectedArticle, articleFromFirebase } = state;
     const { slug, post } = selectedArticle;
-
+    const { isFetching } = articleFromFirebase;
     return {
-        slug, 
-        post
+        slug,
+        post,
+        isFetching
     }
 }
 export default connect(mapStateToProps)(ArticlePage);
