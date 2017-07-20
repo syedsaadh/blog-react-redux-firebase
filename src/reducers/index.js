9e9f872316cd824be6219f4d12e5ccadc7d9d1ae
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-21 04:22:15 +0530

    Failed Receive action case added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,8 +6,8 @@ import {
     REQUEST_ALL_POSTS,
     FAILED_RECEIVE_POSTS,
     SELECT_ARTICLE,
-    REQUEST_ARTICLE, 
-    RECEIVED_ARTICLE, 
+    REQUEST_ARTICLE,
+    RECEIVED_ARTICLE,
     FAILED_RECEIVE_ARTICLE,
     NOT_FOUND_ARTICLE
 } from '../actions'
@@ -30,6 +30,9 @@ const postsFromFirebase = (state = initialState, action) => {
                 items: action.payload,
                 lastUpdated: action.receivedAt
             }
+        case FAILED_RECEIVE_POSTS: {
+            return { ...state, isFetching: false }
+        }
         default:
             return state
     }
@@ -52,21 +55,24 @@ const selectedArticle = (state = initialSelectedPost, action) => {
 }
 const initialArticleFromFirebase = {
     isFetching: false,
+    notFound: false,
     post: {}
 }
 const articleFromFirebase = (state = initialArticleFromFirebase, action) => {
     switch (action.type) {
         case REQUEST_ARTICLE: {
-            return {...state, isFetching: true}
+            return { ...state, isFetching: true }
         }
         case RECEIVED_ARTICLE: {
-            return {...state, isFetching:false, post: action.post}
+            return { ...state, isFetching: false, post: action.post }
+        }
+        case FAILED_RECEIVE_ARTICLE: {
+            return { ...state, isFetching: false }
         }
         case NOT_FOUND_ARTICLE: {
-            alert('Not Found')
-            return state;
+            return { ...state, isFetching: false, notFound: true };
         }
-        default: 
+        default:
             return state;
     }
 }
