85478614c5a8193ec81fd28cc958a609af5d9556
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-21 04:20:42 +0530

    removed unused vars

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux'
 import { Route, Switch, Link } from 'react-router-dom';
 import App from './containers/App';
 import ArticlePage from './containers/ArticlePage';
+import NoMatch from './containers/NoMatch';
 
 import configureStore from './store/configureStore'
 import DevTools from './containers/DevTools'
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { ConnectedRouter } from 'react-router-redux'
 import { createBrowserHistory } from 'history'
 var store = configureStore();
 
 var history = createBrowserHistory();
-
 
 render(
   <Provider store={store} >
@@ -20,9 +20,13 @@ render(
       <DevTools />
       <ConnectedRouter history={history}>
         <div>
-          <Link to="/">Home</Link>
-          <Route exact path="/" component={App} />
-          <Route path="/post/:slug" component={ArticlePage} />
+          <Link to="/">Home</Link>&nbsp;&nbsp;
+          <Link to="/sda">No Match</Link>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/post/:slug" component={ArticlePage} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
       </ConnectedRouter>
     </div>
