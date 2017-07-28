d2e9976538ce53255c2065eec6cd86e4107562fa
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-28 23:23:17 +0530

    Navbar Component and Styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,52 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-export default class Navbar extends Component {
-    render(){
-        return(
-            <nav className="navbar navbar-default" role="navigation">
-                  <div className="container-fluid">
-                    <div className="navbar-header">
-                      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                      </button>
-                      <Link className="navbar-brand" to="/">ReactRedux Blog</Link>
-                    </div>
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      <ul className="nav navbar-nav">
-                        <li><Link to="/sdasd">No Match</Link></li>
-                        <li><Link to="#">Link</Link></li>
-                      </ul>
-                      <form className="navbar-form navbar-right" role="search">
-                        <div className="form-search search-only">
-                          <i className="search-icon glyphicon glyphicon-search"></i>
-                          <input type="text" className="form-control search-query"/>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </nav>
-        )
+import { connect } from 'react-redux';
+import { deauthenticate, authenticate } from '../actions/sessionActions';
+import '../styles/Navbar.css';
+class Navbar extends Component {
+  handleSignIn() {
+    const { dispatch } = this.props;
+    dispatch(authenticate());
+  }
+  handleLogout() {
+     const { dispatch } = this.props;
+     dispatch(deauthenticate())
+  }
+  render() {
+    const { session } = this.props;
+    let button = null;
+    if (!session) {
+      button = <a onClick={this.handleSignIn.bind(this)}>LOGIN</a>
+    } else {
+      button = <a onClick={this.handleLogout.bind(this)}>LOGOUT</a>
     }
+    return (
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <Link className="navbar-brand" to="/">ReactRedux Blog</Link>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav navbar-right">
+              {session && <li><Link to="/dashboard">DASHBOARD</Link></li>}
+              <li>{button}</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
+const mapStateToProps = state => {
+  const { session } = state;
+  return {
+    session
+  }
+}
+export default connect(mapStateToProps)(Navbar);
