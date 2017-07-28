c185eceac3f2f0492949a1d5e4abde72d84fe031
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-29 02:24:53 +0530

    Include Date View in Post Highlight

--- src/components/PostHighlight.js
+++ src/components/PostHighlight.js
@@ -11,7 +11,10 @@ export default class PostHighlight extends Component {
             <article className="post-highlight">
                 <header>
                     <span className="post-meta"></span>
-                    <h2 className="post-title"><Link to={`/post/${data.slug}`} onClick={ e => onPostClick(data) }>{data.title}</Link></h2>
+                    <h2 className="post-title">
+                        <Link to={`/post/${data.slug}`} onClick={ e => onPostClick(data) }>{data.title}</Link>
+                        <span className="date">12 AUG 2017</span>
+                    </h2>
                 </header>
                 <section className="post-excerpt">
                     <p>{truncate(removeMd(data.desc), {'length':220})}</p>
