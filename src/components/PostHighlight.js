90a4dca9e22a35fb099b854145e1acf09b70ce9c
xeda <hassan.saad.mail@gmail.com>
2017-08-03 23:40:16 +0530

    Tags Element added

--- src/components/PostHighlight.js
+++ src/components/PostHighlight.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import '../styles/PostHighlight.css';
 import {Link} from 'react-router-dom';
+import {each as _each} from 'lodash';
 import {truncate} from 'lodash';
+import {Tag} from 'element-react';
 import * as removeMd from "remove-markdown";
 
+import '../styles/PostHighlight.css';
+import 'element-theme-default';
+
 export default class PostHighlight extends Component {
+    createTagList(tags) {
+        if(!tags){
+            return false;
+        }
+        return tags.map((name, key) => {
+            return <Tag key={key} type="gray" style={{marginRight: '10px'}}>{ name }</Tag>
+        })
+    }
     render() {
         const { data, onPostClick } = this.props;
         return (
@@ -18,6 +30,7 @@ export default class PostHighlight extends Component {
                 </header>
                 <section className="post-excerpt">
                     <p>{truncate(removeMd(data.desc), {'length':220})}</p>
+                    { this.createTagList(data.tags || null) }
                 </section>
                 
             </article>
